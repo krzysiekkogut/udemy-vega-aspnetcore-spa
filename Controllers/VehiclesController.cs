909d7157d2c06532168f1c9b11aacaa94866e8ce
Krzysztof Kogut <krzysztof@kogutprogramuje.pl>
2020-03-29 20:33:04 +0200

    Add delete vehicle endpoint

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -55,11 +55,31 @@ namespace udemy_vega_aspnetcore_spa.Controllers
       }
 
       var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+      if (vehicle == null)
+      {
+        return NotFound();
+      }
+
       mapper.Map<VehicleApiDto, Vehicle>(vehicleApiDto, vehicle);
       vehicle.LastUpdate = DateTime.UtcNow;
       await context.SaveChangesAsync();
       var vehicleResponse = mapper.Map<VehicleApiDto>(vehicle);
       return Ok(vehicleResponse);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteVehicle(int id)
+    {
+      var vehicle = await context.Vehicles.FindAsync(id);
+      if (vehicle == null)
+      {
+        return NotFound();
+      }
+
+      context.Remove(vehicle);
+      await context.SaveChangesAsync();
+      return Ok(id);
     }
   }
 }
